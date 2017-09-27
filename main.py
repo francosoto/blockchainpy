2fd3dbfff123ac89cf40999a2dc1632733f8f9fe
Franco <franco.soto.z@gmail.com>
2017-09-27 02:15:30 -0300

    Hash function added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import redis
+import redis, hashlib, json, sys
+# I dont know where are your Redis server! 
 r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
-r.set('foo', 'bar')
+
+
+# r.set('foo', 'bar')
+
+def hashMe(msg=""):
+    # For convenience, this is a helper function that wraps our hashing algorithm
+    if type(msg)!=str:
+        msg = json.dumps(msg,sort_keys=True)  # If we don't sort keys, we can't guarantee repeatability!
+        
+    if sys.version_info.major == 2:
+        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
+    else:
+        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
